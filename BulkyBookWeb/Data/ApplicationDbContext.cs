92512b9c9065ab8ab2b3f01ae5ee939890f1f1d2
Janarthanan S <janarthanansubburaj2001@gmail.com>
2026-08-30 11:09:59 +0530

    Changes 1#

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -8,5 +8,6 @@ namespace BulkyBookWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
 
         public DbSet<Category>Categories { get; set; }
+
     }
 }
